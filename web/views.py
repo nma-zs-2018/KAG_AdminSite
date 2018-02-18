3794f7cab0c02bdbbb0bc854880736b463eefaf8
Simas Kuprelis <simas.kuprelis@gmail.com>
2018-02-19 00:06:55 +0200

    add allowed host

--- web/views.py
+++ web/views.py
@@ -1,7 +1,16 @@
+import firebase_admin
 from django.shortcuts import render
 
 # Create your views here.
+from firebase_admin import credentials, db
 
 
 def index(request):
-    return render(request, 'index.html')
+    cred = credentials.Certificate('web/serviceKey.json')
+    try:
+        app = firebase_admin.get_app()
+    except ValueError:
+        app = firebase_admin.initialize_app(cred, {'databaseURL': 'https://sandbox-c9049.firebaseio.com/'})
+    ref = db.reference('/test', app)
+    json = ref.get()
+    return render(request, 'index.html', {'name': json})
